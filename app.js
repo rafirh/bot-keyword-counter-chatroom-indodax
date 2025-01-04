909328bed493556db8db8ad6f40a54dc2e7d2984
rafirh <rafirahmann18@gmail.com>
2025-01-04 09:23:27 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// endpoint: https://indodax.com/api/v2/chatroom/history
+// save to: chatroom.json every 10 seconds
+
+// {
+//   "success": true,
+//   "code": 200,
+//   "message": "Chatroom connected",
+//   "data": {
+//       "content": [
+//           {
+//               "id": 45770751,
+//               "username": "Irma_nurmalasari",
+//               "content": "@Muhammad_Zealot ntr jual 7k ga tau kapan bntr lagi",
+//               "timestamp": 1735955759,
+//               "level": 3
+//           },
+
+// just save the data.content to chatroom.json
+
+const axios = require('axios');
+const fs = require('fs');
+
+const endpoint = 'https://indodax.com/api/v2/chatroom/history';
+const saveTo = 'chatroom.json';
+const keyword = 'cukongindodax';
+
+const saveChatroom = async () => {
+  try {
+    const response = await axios.get(endpoint);
+    const chatroom = response.data.data.content;
+
+    let chatroomData = [];
+    if (fs.existsSync(saveTo)) {
+      const fileContent = fs.readFileSync(saveTo);
+      if (fileContent.length > 0) {
+        chatroomData = JSON.parse(fileContent);
+      } else {
+        chatroomData = [];
+      }
+    }
+
+    chatroom.forEach((chat) => {
+      const isExist = chatroomData.find(
+        (data) => data.id === chat.id
+      );
+
+      if (!isExist && chat.content.toLowerCase().includes(keyword.toLowerCase())) {
+        chatroomData.unshift(chat);
+      }
+    })
+
+    fs.writeFileSync(saveTo, JSON.stringify(chatroomData, null, 2));
+
+    console.log('Chatroom saved');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+saveChatroom();
+setInterval(saveChatroom, 10000);
